a2b50998490d65e06cea775c42bb66564e3ca3b2
Joshua Philips <jmichaelphilips@gmail.com>
2016-11-09 17:34:03 -0600

    added the third contains? function

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,10 +1,18 @@
 defmodule Cards do
 
     def create_deck do 
-      ["Ace", "2", "3"]
+      ["Ace", "Two", "Three", "Four", "Five"]
     end 
 
     def shuffle(deck) do
-         
+      Enum.shuffle(deck)  
     end 
+
+    def contains?(deck, card) do 
+      Enum.member?(deck, card)  
+    end
+
+
+
+
 end
