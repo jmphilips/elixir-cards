4e188cc09d6b1028d832448fd825a955c89e0ed2
Joshua Philips <jmichaelphilips@gmail.com>
2016-11-10 21:03:11 -0600

    Wrote a test

--- lib/cards.ex
+++ lib/cards.ex
@@ -21,6 +21,17 @@ defmodule Cards do
       Enum.shuffle(deck)  
     end 
 
+    @doc """ 
+      Determins whether a deck contains a given card
+
+    ## Examples
+
+        iex> deck = Cards.create_deck 
+        iex> Cards.contains?(deck, "Ace of Spades")
+        true
+
+    """
+
     def contains?(deck, card) do 
       Enum.member?(deck, card)  
     end
@@ -31,7 +42,7 @@ defmodule Cards do
       The `hand_size` argument indicates how many cards 
       should be in the hand.
 
-      ## Examples
+    ## Examples
         iex> deck = Cards.create_deck
         iex> {hand, deck} = Cards.deal(deck, 1)
         iex> hand
